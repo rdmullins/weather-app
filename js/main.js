0c1b282687ff57afabf16d50c6dd65b70ccb41ae
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-28 14:53:08 +0000

    Working on API call function

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
-let weatherData = "";
+// Weather App
+// by Roger Mullins
 
-async function apiTest() {
-    const dataBack = await axios.get("https://api.openweathermap.org/data/2.5/weather?zip=40504,us&appid=a6063ef55e23ba5afdf671e31f5cdf4b");
-    console.log("Inside the async function, dataBack = ", dataBack);
-    weatherData = dataBack;
-    objectIn(weatherData);
+// Object Definition
+
+let stateObj = {
+  city : "",
+  zipCode: "",
+  tempK: 0,
+  tempF: 0,
+  tempC: 0,
+  condition: "",
+  imageURL: ""
 };
 
-apiTest();
+async function callAPI() {
+  // Gets data from API and updates stateObj
+  const dataBack = await axios.get("https://api.openweathermap.org/data/2.5/weather?zip=40504,us&appid=a6063ef55e23ba5afdf671e31f5cdf4b");
+  console.log("Inside the async function, dataBack = ", dataBack);
+  console.log("City is: ", dataBack.data.name);
+  console.log("ZIP code is: ", "40504 <- this is hard coded for now");
+  console.log("Temperature (K) is: ", dataBack.data.main.temp, "K");
+  console.log("Temperature (F) is: ", (1.8*((dataBack.data.main.temp)-273)), "°F");   // 1.8*(K-273) + 32
+  console.log("Temperature (C) is: ", (databack.data.main.temp - 273.15), "°C")       // Kelvin – 273.15
+  console.log("")
+  // weatherData = dataBack;
+  // objectIn(weatherData);
+};
 
-console.log("In the main program, weatherData = ", weatherData);
+callAPI();
 
-function objectIn(param) {
-    console.log("In the objectIn function, weatherData = ", param);
-}
+// console.log("In the main program, weatherData = ", weatherData);
 
-async function getData(url) {
-    //try {
-      const response = await axios.get(url);
-      console.log(response.data.results);
-    //} catch (error) {
-    //  console.error(error);
-    //}
-  }
+// function objectIn(param) {
+//     console.log("In the objectIn function, weatherData = ", param);
+// }
+
+// async function getData(url) {
+//     //try {
+//       const response = await axios.get(url);
+//       console.log(response.data.results);
+//     //} catch (error) {
+//     //  console.error(error);
+//     //}
+//   }
   
-  getData('https://rickandmortyapi.com/api/character');
+//   getData('https://rickandmortyapi.com/api/character');
