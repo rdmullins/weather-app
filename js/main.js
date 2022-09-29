0e64fa41a2536b721c82897d854e67daaaa728a7
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-29 14:57:25 +0000

    Added date and time

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 let stateObj = {
   city : "",
   zipCode: "",
+  currentTime: "",
   tempK: 0,
   tempF: 0,
   tempC: 0,
@@ -86,29 +87,62 @@ function buildUI() {
   createElement("returned-data-section", "div", ["row", "bg-warning", "align-items-center"], "city-label-row");
   createElement("city-label-row", "div", ["col", "display-5"], "city-label-display", "City");
   createElement("returned-data-section", "div", ["row", "align-items-center"], "city-display-row");
-  createElement("city-display-row", "div", ["col", "align-items-center", "display-3"], "city-display", "CITY GOES HERE");              // city-display
+  createElement("city-display-row", "div", ["col", "align-items-center", "display-3"], "city-display", "CITY GOES HERE");   // city-display
   createElement("returned-data-section", "div", ["row"], "zip-code-display-row");
-  createElement("zip-code-display-row", "div", ["col", "display-5"], "zip-code-display", "ZIP GOES HERE");       // zip-code-display
+  createElement("zip-code-display-row", "div", ["col", "display-5"], "zip-code-display");                                   // zip-code-display
+  createElement("returned-data-section", "div", ["row"], "time-and-date-row");
+  createElement("time-and-date-row", "div", ["col-6", "display-5"], "showDate");
+  createElement("time-and-date-row", "div", ["col-6", "display-5"], "showTime");
   createElement("appContainer", "div", ["row", "text-center", "bg-warning"], "temperature-label-row");
   createElement("appContainer", "div", ["row", "text-center", "bg-warning"], "temperature-display-label-row");
   createElement("temperature-display-label-row", "div", ["col", "display-5"], "temperature-display-label", "Temperature");
   createElement("appContainer", "div", ["row"], "temperature-display-row");
-  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-6"], "temp-Kelvin", "KELVIN GOES HERE");          // temp-Kelvin
-  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-3", "bg-info"], "temp-Fahrenheit", "FAHRENHEIT GOES HERE");  // temp-Fahrenheit
-  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-6"], "temp-Celsius", "CELSIUS GOES HERE");        // temp-Celsius
+  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-6"], "temp-Kelvin");                    // temp-Kelvin
+  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-3", "bg-info"], "temp-Fahrenheit");     // temp-Fahrenheit
+  createElement("temperature-display-row", "div", ["col-4", "text-center", "display-6"], "temp-Celsius");                   // temp-Celsius
   createElement("appContainer", "div", ["row", "text-center", "bg-warning"], "condition-label-row");
   createElement("condition-label-row", "div", ["col", "display-5"], "current-conditions-label", "Current Conditions");
   createElement("appContainer", "div", ["row"], "condition-display-row");
-  createElement("condition-display-row", "div", ["col", "text-center", "display-5"], "condition-display",);         // condition-display
+  createElement("condition-display-row", "div", ["col", "text-center", "display-5"], "condition-display",);                 // condition-display
   createElement("appContainer", "div", ["row"], "other-info-label-row");
   createElement("other-info-label-row", "div", ["col", "display-5"], "other-info-label");
   createElement("appContainer", "div", ["row"], "weather-img-row");
   //createElement("other-info-display-row", "div", ["row"], "weather-img-row");
   createElement("weather-img-row", "div", ["col", "text-center", "bg-info"], "weather-img-col");
-  createElement("weather-img-col", "img", [], "weather-img");                                      // weather-img
+  createElement("weather-img-col", "img", [], "weather-img");                                                               // weather-img
   createElement("main", "footer", [], "footer-info");
   createElement("footer-info", "h6", ["text-center"], "favicon-attribution", "FavIcon provided by https://www.flaticon.com/free-icons/weather");
 };
+
+function getTime() {
+  // Reused from alarm clock project -RM
+  let timeHours = 0;
+  let timeMinutes = 0;
+  let timeSeconds = 0;
+  let alarmHours = 0;
+  let alarmMinutes = 0;
+  let alarmSeconds = 0;
+  
+
+  let today = new Date();
+  let time = today.getHours() + ":";
+  if (today.getMinutes() < 10) {
+      time = time + "0";
+  }
+  time = time + today.getMinutes() + ":";
+  if (today.getSeconds() < 10) {
+      time = time + "0";
+  }
+  time = time + today.getSeconds();
+  let date = (today.getMonth()+1)+'-'+today.getDate() +'-'+ today.getFullYear();
+
+  let timeBox = document.getElementById("showTime");
+  let dateBox = document.getElementById("showDate");
+  timeBox.innerText = time;
+  dateBox.innerText = date;
+
+  //return (date + " | " + time);
+}
 
 async function callAPI(zipIn) {
   // Builds API call string and gets data from API
@@ -125,6 +159,7 @@ async function callAPI(zipIn) {
   // Update stateObj with returned data
   stateObj.city = dataBack.data.name;
   stateObj.zipCode = zipIn;
+  //stateObj.currentTime = setInterval(getTime, 1000);
   stateObj.tempK = (dataBack.data.main.temp+"K"); 
   stateObj.tempF = (Math.floor((1.8*((dataBack.data.main.temp)-273))+32)+"°F");   // 1.8*(K-273) + 32 
   stateObj.tempC = (Math.floor((dataBack.data.main.temp - 273.15))+"°C");         // Kelvin – 273.15
@@ -179,6 +214,7 @@ function zipValidates(zipToCheck) {
 };
 
 buildUI();
+setInterval(getTime, 1000);
 zipIn = prompt("ZIP Code:");
 
 if (zipValidates(zipIn)) {
